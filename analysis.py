965787a9be909eefb5885f45903239beef8305e0
Ejohns14 <ejohns@gmail.com>
2022-11-02 22:31:04 -0400

    Modified analysis file, now does analysis

--- analysis.py
+++ analysis.py
@@ -1,15 +1,43 @@
 import csv
 
 def get_highest_gdp(data, year):
-    pass
+    highest_gdp_state = data[0]["GeoName"]
+    maximum = float(data[0][year])
+    for d in data:
+        # if that value is greater than the max I've seen so far
+        value = float(d[year])
+        if value < maximum:
+            continue
+            # set it to be my new max
+        if value > maximum:
+            maximum = value
+            highest_gdp_state = d["GeoName"]
+    return highest_gdp_state
+
 
 def get_lowest_gdp(data, year):
-    pass
+    lowest_gdp_state = data[0]["GeoName"]
+    minimum = float(data[0][year])
+    for d in data:
+        # if that value is greater than the max I've seen so far
+        value = float(d[year])
+        if value > minimum:
+            continue
+            # set it to be my new max
+        if value < minimum:
+            minimum = value
+            lowest_gdp_state = d["GeoName"]
+        return lowest_gdp_state
+
 def get_state_gdp(data, state, year):
-    pass
+    for row in data:
+        if row["GeoName"] == state:
+            return row[year]
+        
+    
 # save each row into a list (TODO: change to your path!)
 list_data = []
-with open("data/state_gdp_analysis.csv", "r") as infile:
+with open("state_gdp_analysis.csv", "r") as infile:
     # load in data as DictReader
     reader = csv.DictReader(infile)
     # go through each year and get highest and lowest gdp
@@ -17,6 +45,7 @@ with open("data/state_gdp_analysis.csv", "r") as infile:
         list_data.append(row)
 
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
-
+print(get_highest_gdp(list_data, '2020'))
 
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
+print(get_lowest_gdp(list_data, '2020'))
